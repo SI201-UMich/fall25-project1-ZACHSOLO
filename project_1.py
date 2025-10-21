688756c5a27e2ee1a3da4feb235fba84fe3c389b
ZACHSOLO <zsolly@umich.edu>
2025-10-20 21:52:37 -0400

    flipper length by sex

--- project_1.py
+++ project_1.py
@@ -1,1 +1,15 @@
 import pandas as pd
+def load_penguin_data(csv_file):
+    try:
+        df = pd.read_csv(csv_file)
+        return df
+    except FileNotFoundError:
+        print(f"Error: The file '{csv_file}' was not found.")
+        return None
+    
+
+
+def average_flipper_length_by_sex(df):
+    df = df.dropna(subset = ['sex', 'flipper_length_mm', 'body_mass_g'])
+    avg_flipper = df.groupby('sex')['flipper_length_mm'].mean().round(2)
+    return avg_flipper.to_dict()
