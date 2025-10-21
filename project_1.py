7a051dade2ddfb0721d93abf67dee9f2108fea2e
ZACHSOLO <zsolly@umich.edu>
2025-10-20 21:58:11 -0400

    max bm by island

--- project_1.py
+++ project_1.py
@@ -13,3 +13,10 @@ def average_flipper_length_by_sex(df):
     df = df.dropna(subset = ['sex', 'flipper_length_mm', 'body_mass_g'])
     avg_flipper = df.groupby('sex')['flipper_length_mm'].mean().round(2)
     return avg_flipper.to_dict()
+
+
+def max_body_mass_by_island(df):
+    df = df.dropna(subset=['island', 'body_mass_g', 'species'])
+    grouped = df.loc[df.groupby('island')['body_mass_g'].idmax(), ['island', 'species', 'body_mass_g']]
+    result = grouped.set_index('island').to_dict(orient='index')
+    return result 
